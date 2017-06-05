0e6f0e5ddfae3ff0fe852aa3c975cfbb606413fa
SAWADA Tadashi <cesare@mayverse.jp>
2017-06-05 23:05:36 +0900

    Accept JSON requests

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+extern crate rustc_serialize;
 #[macro_use]
 extern crate nickel;
 
-use nickel::Nickel;
+use nickel::{JsonBody, HttpRouter, Nickel};
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct Person {
+    firstname: String,
+    lastname: String,
+}
 
 fn main() {
     let mut server = Nickel::new();
 
-    server.utilize(router! {
-        get "**" => |_req, _res| {
-            "Hello, world!"
-        }
+    server.post("/people",
+                middleware! {|request, response|
+        let person = request.json_as::<Person>().unwrap();
+        format!("Hello, {} {}", person.firstname, person.lastname)
     });
 
     server.listen("127.0.0.1:6767");
